8aad79cc11dd8ccde5b7ec05021c7296aa531549
Admir Muhic <admir.muhic@outlook.com>
2023-08-17 12:20:43 +0200

    Pinia added

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
@@ -6,8 +7,10 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
+const pinia = createPinia()
 const app = createApp(App)
 
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
